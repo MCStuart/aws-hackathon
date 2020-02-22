0a0a53d9433ac7b432c76cdcb6e052ea8bce76e5
MCStuart <stuart51994@gmail.com>
2020-02-22 15:30:07 -0800

    create splashpahe hero image with styles

--- src/Homepage/homepage.js
+++ src/Homepage/homepage.js
@@ -1,9 +1,20 @@
 import React from 'react';
+import './homepage.css';
+import '../Resource/stacked-rocks.jpg';
 
 export default function Homepage() {
     return (
         <div>
-            
+            <section className="fuzz">
+                <div className="hero-inner">
+                    <div className="titleSplash">
+                        <h1>Happy Health</h1>
+                        <button id="splashButton" className="btn">Login</button>
+                        <button id="splashButton" className="btn">Sign Up</button>
+                    </div>
+                </div>
+            </section>
         </div>
     )
 }
+                                                    
