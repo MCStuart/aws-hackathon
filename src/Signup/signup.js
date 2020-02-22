55281569caf02cc474763959d2b582b35d214272
MCStuart <stuart51994@gmail.com>
2020-02-22 15:29:16 -0800

    create seperate singup component

---
+++ src/Signup/signup.js
@@ -0,0 +1,26 @@
+import React from 'react';
+
+export default function signup() {
+    return (
+        <div>
+            <main className="registerScreen">
+                <h1>Register</h1>
+                <div className="dataInput">
+                    <div className="form-group">
+                        <label htmlFor="user-email">email address</label>
+                        <input type="email" className="form-control" id="user-email" aria-describedby="emailHelp" placeholder="enter email" />
+                    </div>
+                    <div className="form-group">
+                        <label htmlFor="user-password">password</label>
+                        <input type="password" className="form-control" id="user-password" placeholder="password" />
+                    </div>
+                    <div className="form-group">
+                        <label htmlFor="user-password">confirm password</label>
+                        <input type="password" className="form-control" id="user-password-confirm" placeholder="confirm password" />
+                    </div>
+                </div>
+                <button id="registerNewUser" className="btn btn-success" type="button" name="button">register</button>
+            </main>
+        </div>
+    )
+}
