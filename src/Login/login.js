fb91c9d6dff218bcb19d73f7c98c293ebca59706
MCStuart <stuart51994@gmail.com>
2020-02-22 17:51:55 -0800

    wrap input form in <form> tags

--- src/Login/login.js
+++ src/Login/login.js
@@ -4,19 +4,21 @@ export default function login() {
     return (
         <div>  
             <main className="loginScreen">
-                <h1>Please Login</h1>
-                <div className="dataInput">
-                    <div className="form-group">
-                        <label htmlFor="user-email">email address</label>
-                        <input type="email" className="form-control user-email" aria-describedby="emailHelp" placeholder="enter email" />
+                <form>
+                    <h1>Please Login</h1>
+                    <div className="dataInput">
+                        <div className="form-group">
+                            <label htmlFor="user-email">email address</label>
+                            <input type="email" className="form-control user-email" aria-describedby="emailHelp" placeholder="enter email" />
+                        </div>
+                        <div className="form-group">
+                            <label htmlFor="user-password">password</label>
+                            <input type="password" className="form-control user-password" placeholder="password" />
+                        </div>
+                        <button id="newUserButton" className="btn-small">click here to register if you are a new user</button>
                     </div>
-                    <div className="form-group">
-                        <label htmlFor="user-password">password</label>
-                        <input type="password" className="form-control user-password" placeholder="password" />
-                    </div>
-                    <button id="newUserButton" className="btn-small">click here to register if you are a new user</button>
-                </div>
-                <button id="login" className="btn" type="button" name="button">login</button>
+                    <button id="login" className="btn" type="button" name="button">login</button>
+                </form>
             </main>
         </div>
     )
